ef3c0d5eb2ae8c1a658156e393bc8de1d7e130ff
ultral70 <labombamende@gmail.com>
2020-10-28 08:11:07 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,49 @@
+//Operadores
+
+//Operadores de Asignación
+
+let a = 5 //el "=" asigna valores
+var b = 10
+b = a // asignamos el valor de "b" a "a"
+
+//Operadores aritméticos
+print(1 + 2) // suma
+print(1 - 1) // resta
+print(2 * 4) // multiplicación
+print(4 / 2) // división
+print(1 % 2) // módulo, resto de la división entera
+
+//OP (operador) negar
+var c = -b //negativo de un número
+
+//OP composición
+b += 10 // b = b + 10
+
+//OP comparadores
+print(1 == 1) // comparamos si es un valor igual a otro
+print(1 != 2) // comparamos si es distinto
+print(2 > 1) // mayor
+print(1 < 2) // menor
+print(2 >= 1) //mayor o igual
+print(1 <= 2) // menor o igual
+
+//OP ternario
+//Operador equivalente a un if-else, más corto
+let anchura = 40
+let tieneCabecera = true
+//Supongamos que la altura de la columna es en función de si tiene cabecera y de la anchurA. En caso de que, si tieneCabecera es true, se ejecuta el código que hay después de la "?", en caso contrario, se ejecuta el código de después del ":"
+var alturaColumna = anchura + (tieneCabecera ? 50 : 20) 
+print(alturaColumna)//90
+
+//operador nil-coalescing  (Nil-fusionado)
+
+//Sintaxis (a ?? b)
+//Se utliliza mucho con optionals
+//En caso de que el optional "a" tenga un valor, entonces devuelve el valor de "a"
+//En caso de que el optional "a" sea nulo, entonces devuelve el valor de "b"
+let colorPorDefecto = "Azul"
+var colorDefinidoPorUsuario : String? //Se inicializa a nil (nulo)
+colorDefinidoPorUsuario = "verde"
+
+var color = (colorDefinidoPorUsuario ?? colorPorDefecto)
+print(color)
